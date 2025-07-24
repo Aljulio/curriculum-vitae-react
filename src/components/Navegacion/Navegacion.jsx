ee42446de5371ad03488428109954c5edbc26e26
Julio Alberto Hernandez Morales <julioalbertoh79@gmail.com>
2025-07-23 21:29:47 -0600

    Actualizar enlace de Educacion en la barra de navegacion

--- src/components/Navegacion/Navegacion.jsx
+++ src/components/Navegacion/Navegacion.jsx
@@ -9,7 +9,7 @@ const Navegacion = () => {
         {/* Si tienes una sección de Objetivo, asegúrate de que su componente tenga id="objetivo" */}
         <li><a href="#objetivo">Objetivo</a></li>
         {/* Si tienes una sección de Formación, asegúrate de que su componente tenga id="formacion" */}
-        <li><a href="#formacion">Formación</a></li>
+        <li><a href="#education">Education</a></li>
         <li><a href="#experiencia">Experiencia</a></li>
         <li><a href="#habilidades">Habilidades</a></li>
         <li><a href="#contacto">Contacto</a></li>
