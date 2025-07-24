e7fdcea41caab8a0416a68b8bfd9f45e94a5f1db
Julio Alberto Hernandez Morales <julioalbertoh79@gmail.com>
2025-07-23 21:47:09 -0600

    Cambiar texto de navegacion a 'Educacion' en español

--- src/components/Navegacion/Navegacion.jsx
+++ src/components/Navegacion/Navegacion.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
-import './Navegacion.css'; // Vamos a crear este archivo CSS también
+import './Navegacion.css';
 
-const Navegacion = () => {
+function Navegacion() {
   return (
     <nav className="main-nav">
       <ul>
         <li><a href="#personal">Personal</a></li>
-        {/* Si tienes una sección de Objetivo, asegúrate de que su componente tenga id="objetivo" */}
         <li><a href="#objetivo">Objetivo</a></li>
-        {/* Si tienes una sección de Formación, asegúrate de que su componente tenga id="formacion" */}
-        <li><a href="#education">Education</a></li>
+        <li><a href="#education">Educación</a></li> {/* Texto cambiado a "Educación" */}
         <li><a href="#experiencia">Experiencia</a></li>
         <li><a href="#habilidades">Habilidades</a></li>
         <li><a href="#contacto">Contacto</a></li>
       </ul>
     </nav>
   );
-};
+}
 
 export default Navegacion;
